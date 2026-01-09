a2a1af1f13e8ce8e19555f2006702b128dab2d7c
bhaskardey <deybhaskar999@gmail.com>
2026-01-10 01:10:59 +0530

    Update iOS configuration and Koin initialization
    -- Updated `Info.plist` with necessary permissions for photo library access (saving and picking images) and defined supported interface orientations and device capabilities.
    -- Modified `initKoin` in `commonMain` to handle nullable app declarations and standardized module loading.
    -- Integrated Koin initialization into the `iOSApp` entry point.

--- composeApp/src/commonMain/kotlin/com/bhaskar/pixelwalls/di/InitKoin.kt
+++ composeApp/src/commonMain/kotlin/com/bhaskar/pixelwalls/di/InitKoin.kt
@@ -3,9 +3,9 @@ package com.bhaskar.pixelwalls.di
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 
-fun initKoin(appDeclaration: KoinAppDeclaration = {}) {
+fun initKoin(appDeclaration: KoinAppDeclaration? = null) {
     startKoin {
-        appDeclaration()
-        modules(appModule, platformModule)
+        appDeclaration?.invoke(this)
+        modules(appModule)
     }
 }
