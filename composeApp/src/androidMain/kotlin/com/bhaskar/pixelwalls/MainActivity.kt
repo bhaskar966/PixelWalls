114c27feee397bf6b97b79e4e7c6b28e89f8c567
bhaskardey <deybhaskar999@gmail.com>
2026-01-11 01:51:28 +0530

    Configure light status bars for Android.
     -- as the app only available in light mode for now.

--- composeApp/src/androidMain/kotlin/com/bhaskar/pixelwalls/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/bhaskar/pixelwalls/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.bhaskar.pixelwalls
 
 import android.os.Bundle
+import android.view.WindowInsetsController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            val window = this.window
+            val controller = WindowInsetsControllerCompat(
+                window,
+                window.decorView
+                )
+
+            controller.isAppearanceLightStatusBars = true
             App()
         }
     }
